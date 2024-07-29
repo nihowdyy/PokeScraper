e40bfaaaac5b473672db7a372aaef03a80c96efe
Gavin Li <76128837+nihowdyy@users.noreply.github.com>
2024-07-29 13:23:45 -0400

    Made the .csv output better looking

--- web_scraper.py
+++ web_scraper.py
@@ -45,14 +45,23 @@ with open('PKMN-SV-TMS.csv', 'w',
         move_name = cells[0].text.strip()
         move_type = cells[1].text.strip()
         move_category = cells[2].text.strip()
-        move_power = cells[3].text.strip()[:3]
+        move_power = str(int(cells[3].text.strip()[:3]))
+        # Making the Move Power cells more nice looking
+        if move_power == '0':
+            move_power = '—'
         move_accuracy = cells[4].text.strip()[:3]
+        # Making the Move Accuracy cells more nice looking
+        if move_accuracy == '—}}':
+            move_accuracy = '0'
+        move_accuracy = str(int(move_accuracy))
+        if move_accuracy == '0':
+            move_accuracy = '—'
         move_pp = cells[5].text.strip()[:3]
         move_location = cells[7].text.strip()
         move_LP = cells[8].text.strip()
         move_materials = cells[9].text.strip()
         sell_price = cells[10].text.strip()
-        data_out = [tm_number, move_name, move_type, move_category, move_power, move_accuracy, move_pp, move_location, move_LP, move_materials, sell_price]
+        data_out = [tm_number, move_name, move_type, move_category, move_power, move_accuracy + '%', move_pp, move_location, move_LP, move_materials, sell_price]
         writer.writerow(data_out)
 
 
