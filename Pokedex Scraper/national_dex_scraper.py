831af7e430132ddab6c760d89232d685586982f4
Gavin Li <76128837+nihowdyy@users.noreply.github.com>
2024-07-31 11:52:16 -0400

    Updated Dex Entries and Scrapers for all Pokemon in SV

--- Pokedex Scraper/national_dex_scraper.py
+++ Pokedex Scraper/national_dex_scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import os
 
 # URL from Bulbapedia
 url = 'https://game8.co/games/Pokemon-Scarlet-Violet/archives/369171'
@@ -8,12 +9,6 @@ page = requests.get(url)
 
 # Beautiful Soup to parse html content
 soup = BeautifulSoup(page.content, "html.parser")
-
-# Second table being the content of the pokemon we want
-table = soup.find_all('table')[1]
-
-# Table Rows
-rows = table.find_all('tr')
 
 # Headers of the information we want
 HEADERS = [
@@ -24,8 +19,23 @@ HEADERS = [
     'Abilities',
 ]
 
-# Writing the csv file to store data to not need multiple calls
-with open('SV_DEX.csv', 'w',
+# Third table being the content of the pokemon we want (Nat. Dex)
+table = soup.find_all('table')[2]
+
+# Table Rows
+rows = table.find_all('tr')
+
+# Directory Path & File Path
+directory = 'CSV Files'
+file_name = 'NAT_DEX.csv'
+file_path = os.path.join(directory, file_name)
+
+# If Directory does not exist, create it
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+# National Dex Pokemon Info
+with open(file_path, 'w',
           newline='', encoding='utf-8') as outfile:
     writer = csv.writer(outfile)
     writer.writerow(HEADERS)
@@ -38,11 +48,11 @@ with open('SV_DEX.csv', 'w',
         cells = row.find_all('td')
         name = cells[0].text.strip()
         image = cells[0].findAll('img')[0]['data-src']
-
+        # Seperate the Pokemon Types
         text_pieces = [str(piece).strip() for piece in cells[1].stripped_strings]
         combined_text = '&'.join(text_pieces)
         type = combined_text
-
+        # Seperate the Pokemon Abilities
         text_pieces = [str(piece).strip() for piece in cells[2].stripped_strings]
         combined_text = '&'.join(text_pieces)
         abilities = combined_text
