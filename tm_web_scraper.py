aad30595885d42daf1bb5419a121043a74ce7903
Gavin Li <76128837+nihowdyy@users.noreply.github.com>
2024-07-31 14:48:55 -0400

    Updated TM Information to Include Images

--- tm_web_scraper.py
+++ tm_web_scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import os
 
 # URL from Bulbapedia
 url = 'https://bulbapedia.bulbagarden.net/wiki/List_of_TMs_in_Pok%C3%A9mon_Scarlet_and_Violet'
@@ -24,14 +25,20 @@ HEADERS = [
     'Power',
     'Accuracy',
     'PP',
+    'IMAGE',
     'Location',
     'LP Cost',
     'Materials Needed',
     'Sell Price',
 ]
 
+# Directory Path & File Path
+directory = 'CSV Files'
+file_name = 'PKMN-SV-TMS.csv'
+file_path = os.path.join(directory, file_name)
+
 # Writing the csv file to store data to not need multiple calls
-with open('PKMN-SV-TMS.csv', 'w',
+with open(file_path, 'w',
           newline='', encoding='utf-8') as outfile:
     writer = csv.writer(outfile)
     writer.writerow(HEADERS)
@@ -57,11 +64,12 @@ with open('PKMN-SV-TMS.csv', 'w',
         if move_accuracy == '0':
             move_accuracy = '—'
         move_pp = cells[5].text.strip()[:3]
+        image = cells[6].findAll('img')[0]['src']
         move_location = cells[7].text.strip()
         move_LP = cells[8].text.strip()
         move_materials = cells[9].text.strip()
         sell_price = cells[10].text.strip()
-        data_out = [tm_number, move_name, move_type, move_category, move_power, move_accuracy + '%', move_pp, move_location, move_LP, move_materials, sell_price]
+        data_out = [tm_number, move_name, move_type, move_category, move_power, move_accuracy + '%', move_pp, image, move_location, move_LP, move_materials, sell_price]
         writer.writerow(data_out)
 
 
